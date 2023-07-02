684576b2ce94131dc3a2cc0c0d1898c52c2d06b3
Jayas Piya <46777167+jayaspiya@users.noreply.github.com>
2023-07-02 16:36:29 +0000

    Updated UI

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 from flask import Flask, request
+from flask_cors import CORS
 from utils import search_engine
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/api', methods=['GET'])
 def search_api():
     query_param = request.args.get('query')
     if query_param is None:
         return 'Error: No string parameter provided', 400
-    results = search_engine(query_param)
-    return results
+    return search_engine(query_param)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
